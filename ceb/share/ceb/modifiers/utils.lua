7aa997ba95810c65f5a34b31981519bde82b10f7
Guillaume Denry <guillaume.denry@gmail.com>
2008-05-26 21:01:32 +0000

    simplification des scripts

--- ceb/share/ceb/modifiers/utils.lua
+++ ceb/share/ceb/modifiers/utils.lua
@@ -5,3 +5,13 @@ function nickInSentence(sentence, nick)
 		(string.find(lowerSentence, "[^%a]"..lowerNick.."[^%w]") ~= nil) or
 		(string.find(lowerSentence, "[^%a]"..lowerNick.."$") ~= nil);
 end
+
+-- Highlight a tell tab if not focused
+function highlightTellTab(peer)
+	local peer_login = tokenArgument(1);
+
+	tab = getTab("tell", peer_login);
+	if not isTabFocused(tab) then
+		setTabColor(tab, "FF0000");
+	end
+end
