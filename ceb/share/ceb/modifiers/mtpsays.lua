efc0bb9b4da1235bbe46d9ad13119c63a15985a0
Guillaume Denry <guillaume.denry@gmail.com>
2008-05-26 07:03:08 +0000

    plus de modifiers

---
+++ ceb/share/ceb/modifiers/mtpsays.lua
@@ -0,0 +1,13 @@
+-- someonesays.lua
+-- By Guillaume Denry (guillaume.denry@gmail.com)
+-- 1) colorize nickname in function of session login
+-- 2) colorize sentence if session login is in
+
+require "utils"
+
+function newToken()
+	tab = getTab("channel");
+	if not isTabFocused(tab) then
+		setTabColor(tab, "0000FF");
+	end
+end
