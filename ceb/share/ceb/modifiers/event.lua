26ccf882c2125c9d75b7f787d92c5246ecfd7176
Kervala <983521+kervala@users.noreply.github.com>
2008-07-25 16:22:22 +0000

    added default event.lua for background colors

---
+++ ceb/share/ceb/modifiers/event.lua
@@ -0,0 +1,22 @@
+-- event.lua
+-- By Kervala
+-- Colorize user background in function of their group
+
+function userBackgroundColor(login)
+
+	groups =
+	{
+		["HeadArch"] = "#ffff00",
+		["SnrGM"] = "#660066",
+		["GM"] = "#ff0000",
+		["VtnGuide"] = "#00ff00",
+		["SnrGuide"] = "#0000ff",
+		["Guide"] = "#ff8000"
+	}
+
+	color = groups[getUserData(login, "group")]
+
+	if color then
+		setUserBackgroundColor(getColor(color, 64))
+	end
+end
