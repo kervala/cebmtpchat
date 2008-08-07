f3483622d92da15db2193278f7913aa7b1ec0403
Kervala <983521+kervala@users.noreply.github.com>
2008-08-07 16:11:40 +0000

    added a handler for mtpchat protocol

---
+++ ceb/src/mtpchat_handler.h
@@ -0,0 +1,36 @@
+/* This file is part of CeB.
+ * Copyright (C) 2005  Guillaume Denry
+ *
+ * CeB is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * CeB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CeB; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef MTPCHAT_HANDLER_H
+#define MTPCHAT_HANDLER_H
+
+#include <QObject>
+#include <QUrl>
+
+class MtpChatHandler : public QObject
+{
+    Q_OBJECT
+
+public:
+    MtpChatHandler();
+
+public slots:
+     void addSession(const QUrl &url);
+};
+
+#endif
