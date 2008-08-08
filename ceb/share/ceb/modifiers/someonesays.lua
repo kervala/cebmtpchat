6611661ec0d07f2188d99349db0e0cb3b5bcdb0f
Guillaume Denry <guillaume.denry@gmail.com>
2008-08-08 00:08:29 +0000

    Issue 68: Only my login in color in sentences about me

--- ceb/share/ceb/modifiers/someonesays.lua
+++ ceb/share/ceb/modifiers/someonesays.lua
@@ -14,8 +14,44 @@ function render()
 	end
 
 	-- Special color for the entire sentence when my nick is in
-	if nickInSentence(getSegmentText(3), login) then
-		setSegmentColor(3, "#FF00FF")
+	sentence = getSegmentText(3)
+	len = string.len(login)
+        t = nickInSentence(sentence, login)
+	f = getSegmentFont(3)
+	s = getSegmentSize(3)
+	c = getSegmentColor(3)
+	off = 1
+	strings = {}
+	startsWithNick = false
+	for i = 1, # t do
+		pos = t[i]
+		if pos > off then
+			table.insert(strings, string.sub(sentence, off, pos - 1))
+		elseif i == 1 then
+			startsWithNick = true
+		end
+		table.insert(strings, string.sub(sentence, pos, pos + len - 1))
+		off = pos + len
+	end
+	if off <= string.len(sentence) then
+		table.insert(strings, string.sub(sentence, off, string.len(sentence)))
+	end
+
+	if # strings > 1 then
+		setSegmentsCount(segmentsCount() + # strings - 1)
+	end
+
+	for i = 1, # strings do
+		setSegmentText(3 + i - 1, strings[i])
+		setSegmentFont(3 + i - 1, f)
+		setSegmentSize(3 + i - 1, s)
+		if startsWithNick then
+			setSegmentColor(3 + i - 1, "#FF00FF")
+			startsWithNick = false
+		else
+			setSegmentColor(3 + i - 1, c)
+			startsWithNick = true
+		end
 	end
 end
 
--- ceb/share/ceb/modifiers/utils.lua
+++ ceb/share/ceb/modifiers/utils.lua
@@ -1,17 +1,55 @@
+function wordLetterBeforePos(sentence, nick, pos)
+	if pos == 1 then
+		return false
+	end
+
+        if not string.find(string.sub(sentence, pos - 1, pos - 1), "%w") then
+		return false
+	end
+
+	return true
+end
+
+function wordLetterAfterPos(sentence, nick, pos)
+	if pos == string.len(sentence) then
+		return false
+	end
+
+        if not string.find(string.sub(sentence, pos + 1, pos + 1), "%w") then
+		return false
+	end
+	return true
+end
+
+function findNick(sentence, nick, init)
+	local lowerSentence, lowerNick = string.lower(sentence), string.lower(nick)
+
+	pos = string.find(lowerSentence, lowerNick, init)
+
+	if (pos ~= nil) and (pos >= 1) and
+          not wordLetterBeforePos(lowerSentence, lowerNick, pos) and
+	  not wordLetterAfterPos(lowerSentence, lowerNick, pos + string.len(lowerNick) - 1) then
+		return pos
+	end
+	return nil
+end
+
 function nickInSentence(sentence, nick)
-    local lowerSentence, lowerNick = string.lower(sentence), string.lower(nick);
-	return (string.find(lowerSentence, "^"..lowerNick.."$") ~= nil) or
-		(string.find(lowerSentence, "^"..lowerNick.."[^%w]") ~= nil) or
-		(string.find(lowerSentence, "[^%a]"..lowerNick.."[^%w]") ~= nil) or
-		(string.find(lowerSentence, "[^%a]"..lowerNick.."$") ~= nil);
+        t = {}
+        b = findNick(sentence, nick, 1, false)
+	while b ~= nil do
+		table.insert(t, b)
+		b = findNick(sentence, nick, b + string.len(nick))
+	end
+	return t
 end
 
 -- Highlight a tell tab if not focused
 function highlightTellTab(peer)
-	local peer_login = tokenArgument(1);
+	local peer_login = tokenArgument(1)
 
-	tab = getTab("tell", peer_login);
+	tab = getTab("tell", peer_login)
 	if not isTabFocused(tab) then
-		setTabColor(tab, "#FF0000");
+		setTabColor(tab, "#FF0000")
 	end
 end
