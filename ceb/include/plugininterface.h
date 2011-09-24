fde1e763027a614d0ada181fd4fcad1aeefedc24
Guillaume Denry <guillaume.denry@gmail.com>
2011-09-24 18:12:29 +0000

    some comments

--- ceb/include/plugininterface.h
+++ ceb/include/plugininterface.h
@@ -8,9 +8,17 @@ class PluginInterface
  public:
      virtual ~PluginInterface() {}
 
+     /** Init the plugin.
+      * Must be called before using getMainWindow().
+      */
      virtual void init() const = 0;
+     /** De-init the plugin.
+      * Must be called when plugin is no more needed.
+      */
      virtual void deinit() const = 0;
 
+     /** Return the single instance mainwindow of ceb QMainWindow
+      */
      virtual QMainWindow *getMainWindow() const = 0;
  };
 
